7145855422d4edcbff7614659b82ac2473d5a773
Andreas Thoelke <andreas.thoelke@gmail.com>
2021-12-24 09:26:22 +0100

    lua notes

---
+++ lua/test1.lua
@@ -0,0 +1,35 @@
+-- lua test
+
+local api = vim.api
+local M = {}
+
+function M.makeScratch()
+  api.nvim_command('enew') -- equivalent to :enew
+  -- vim.bo[0].buftype=nofile -- set the current buffer's (buffer 0) buftype to nofile
+  vim.bo[0].bufhidden=hide
+  vim.bo[0].swapfile=false
+end
+
+
+function M.abc()
+  return 'hi there'
+end
+
+
+
+return M
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
