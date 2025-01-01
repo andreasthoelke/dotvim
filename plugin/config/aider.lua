a2c7788af03616d12973c8209ece0f7cb176a403
Andreas Thoelke <andreas.thoelke@gmail.com>
2025-01-01 19:00:15 +0100

    refactor: Improve Aider ignore update function with debug comment

--- plugin/config/aider.lua
+++ plugin/config/aider.lua
@@ -37,7 +37,7 @@ require('aider').setup({
 -- # AUTOUPDATED by _G.Aider_updateAiderIgnore()
 -- actually you can replace all lines until the end of the file with the new lines.
 
--- debug this. currently only one line is added: ""
+-- debug _G.Aider_updateAiderIgnore(). currently only a single line is added "!/*.*" while the relative paths are not added.
 function _G.Aider_updateAiderIgnore()
     -- Get open folders
     local folders = _G.Ntree_getOpenFolders()
