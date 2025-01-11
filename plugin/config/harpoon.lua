20fe7f1773d6f7e9cf209826efd0ac6f2a3722e0
Andreas Thoelke <andreas.thoelke@gmail.com>
2025-01-11 16:55:50 +0100

    feat: Add custom Harpoon list configuration for command execution

--- plugin/config/harpoon.lua
+++ plugin/config/harpoon.lua
@@ -2,7 +2,45 @@
 local harpoon = require("harpoon")
 -- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
 
-harpoon:setup()
+harpoon:setup({
+    -- Setting up custom behavior for a list named "cmd"
+    "cmd" = {
+
+        -- When you call list:add() this function is called and the return
+        -- value will be put in the list at the end.
+        --
+        -- which means same behavior for prepend except where in the list the
+        -- return value is added
+        --
+        -- @param possible_value string only passed in when you alter the ui manual
+        add = function(possible_value)
+            -- get the current line idx
+            local idx = vim.fn.line(".")
+
+            -- read the current line
+            local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
+            if cmd == nil then
+                return nil
+            end
+
+            return {
+                value = cmd,
+                context = { ... any data you want ... },
+            }
+        end,
+
+        --- This function gets invoked with the options being passed in from
+        --- list:select(index, <...options...>)
+        --- @param list_item {value: any, context: any}
+        --- @param list { ... }
+        --- @param option any
+        select = function(list_item, list, option)
+            -- WOAH, IS THIS HTMX LEVEL XSS ATTACK??
+            vim.cmd(list_item.value)
+        end
+
+    }
+})
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>bb", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
@@ -25,19 +63,19 @@ vim.keymap.set("n", "[a", function() harpoon:list():next() end)
 -- basic telescope configuration
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
+  require("telescope.pickers").new({}, {
+    prompt_title = "Harpoon",
+    finder = require("telescope.finders").new_table({
+      results = file_paths,
+    }),
+    previewer = conf.file_previewer({}),
+    sorter = conf.generic_sorter({}),
+  }):find()
 end
 
 vim.keymap.set("n", "<leader>abb", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
