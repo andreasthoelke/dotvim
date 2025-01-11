58699d77f2d1d0ad7fb30e2ef7dd0f7333b0910c
Andreas Thoelke <andreas.thoelke@gmail.com>
2025-01-11 17:27:21 +0100

    refactor: Update Harpoon configuration with debug prints and modified keymaps

--- plugin/config/harpoon.lua
+++ plugin/config/harpoon.lua
@@ -4,43 +4,46 @@ local harpoon = require("harpoon")
 
 harpoon:setup({
     -- Setting up custom behavior for a list named "cmd"
-    cmd = {
+  cmd = {
 
-        -- When you call list:add() this function is called and the return
-        -- value will be put in the list at the end.
-        --
-        -- which means same behavior for prepend except where in the list the
-        -- return value is added
-        --
-        -- @param possible_value string only passed in when you alter the ui manual
-        add = function(possible_value)
-            -- get the current line idx
-            local idx = vim.fn.line(".")
+    -- When you call list:add() this function is called and the return
+    -- value will be put in the list at the end.
+    --
+    -- which means same behavior for prepend except where in the list the
+    -- return value is added
+    --
+    -- @param possible_value string only passed in when you alter the ui manual
+    add = function(possible_value)
+      -- get the current line idx
+      local idx = vim.fn.line(".")
+      print( 'is this?')
+      -- read the current line
+      local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
+      if cmd == nil then
+        return nil
+      end
 
-            -- read the current line
-            local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
-            if cmd == nil then
-                return nil
-            end
+      return {
+        value = cmd,
+        context = {"hi there"}, -- can store any additional data you want
+      }
+    end,
 
-            return {
-                value = cmd,
-                context = {"hi there"}, -- can store any additional data you want
-            }
-        end,
-
-        --- This function gets invoked with the options being passed in from
-        --- list:select(index, <...options...>)
-        --- @param list_item {value: any, context: any}
-        --- @param list { ... }
-        --- @param option any
-        select = function(list_item, list, option)
-            -- WOAH, IS THIS HTMX LEVEL XSS ATTACK??
-            vim.cmd(list_item.value)
-        end
-
-    }
+    --- This function gets invoked with the options being passed in from
+    --- list:select(index, <...options...>)
+    --- @param list_item {value: any, context: any}
+    --- @param list { ... }
+    --- @param option any
+    select = function(list_item, list, option)
+      -- vim.notify("You selected me: " .. list_item.value)
+      -- vim.cmd(list_item.value)
+    end
+  }
 })
+
+-- print(vim.inspect(require("harpoon"):list("cmd")))
+-- require("harpoon"):list():select(1)
+-- require("harpoon"):list("cmd"):select(1)
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<localleader>a", function() harpoon:list("cmd"):add() end)
@@ -54,8 +57,10 @@ vim.keymap.set("n", ",4", function() harpoon:list():select(4) end)
 vim.keymap.set("n", ",5", function() harpoon:list():select(5) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "]a", function() harpoon:list():prev() end)
-vim.keymap.set("n", "[a", function() harpoon:list():next() end)
+-- vim.keymap.set("n", "]a", function() harpoon:list():prev() end)
+-- vim.keymap.set("n", "[a", function() harpoon:list():next() end)
+vim.keymap.set("n", "]a", function() harpoon:list("cmd"):prev() end)
+vim.keymap.set("n", "[a", function() harpoon:list("cmd"):next() end)
 
 
 -- Test commands for default list:
