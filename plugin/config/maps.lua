4d0b833116856d62c0fa79b61c532676d99374bc
Andreas Thoelke <andreas.thoelke@gmail.com>
2025-11-09 11:20:29 +0100

    remove vim-cheat-sheet2

--- plugin/config/maps.lua
+++ plugin/config/maps.lua
@@ -447,8 +447,8 @@ vim.keymap.set( 'n',
 
 vim.keymap.set( 'n',
   '<leader>gD', function()
-   require'git_commits_viewer'.ShowCurrentFile(5)
-  end, { desc = "Show last 5 git commits of the current file. E.i. the file history of the current file." } )
+   require'git_commits_viewer'.ShowCurrentFile(15)
+  end, { desc = "Show last 15 git commits of the current file. E.i. the file history of the current file." } )
 
 
 vim.keymap.set( 'n',
